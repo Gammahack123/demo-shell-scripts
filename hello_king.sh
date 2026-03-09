6a5003fe2a27d6f5c682c907db0f4b6a81ce4f6c
Gammahack123 <sudhanshukumarmalakar@gmail.com>
2026-03-09 13:18:30 +0000

    added

---
+++ hello_king.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+
+echo "king: hello this sudhanshu kumar king"
+
+echo "king of this world "
+
+echo "ram is the supreme god"
+
+
+echo " every one may fail but no one can defeat the king"
+
+
+mkdir -p rancho
+
+echo "kumar kumar"
+echo "king of this world"
+
+
+
+echo "farahn abba nahi manege" > bittu.txt
+
